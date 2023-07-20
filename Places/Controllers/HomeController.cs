f6f5cd020b49fd01914a7e6667d6228e794b5027
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-20 10:20:14 -0700

    Add photos route

--- Places/Controllers/HomeController.cs
+++ Places/Controllers/HomeController.cs
@@ -11,5 +11,11 @@ namespace Places.Controllers
             return View();
         }
 
+        [Route("/favorite_photos")]
+        public ActionResult FavoritePhotos()
+        {
+            return View();
+        }
+
     }
 }
