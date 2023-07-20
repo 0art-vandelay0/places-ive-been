bc276c67d38482c35aee717444b4f93ebc39964c
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-20 10:20:05 -0700

    Update to add Parent Category

--- Places/Controllers/DestinationsController.cs
+++ Places/Controllers/DestinationsController.cs
@@ -14,11 +14,18 @@ namespace Places.Controllers
             return View(allDestinations);
         }
 
-        [HttpGet("/destinations/new")]
-        public ActionResult New()
+        [HttpGet("/categories/{categoryId}/destinations/new")]
+        public ActionResult New(int categoryId)
         {
-            return View();
+            Category category = Category.Find(categoryId);
+            return View(category);
         }
+
+        // [HttpGet("/destinations/new")]
+        // public ActionResult New()
+        // {
+        //     return View();
+        // }
 
         [HttpPost("/destinations/delete")]
         public ActionResult DeleteAll()
@@ -27,19 +34,29 @@ namespace Places.Controllers
             return View();
         }
 
-        [HttpPost("/destinations")]
-        public ActionResult Create(string cityName, string description)
-        {
-            Destination myDestination = new Destination(cityName, description);
-            return RedirectToAction("Index");
-        }
+        // [HttpPost("/destinations")]
+        // public ActionResult Create(string cityName, string description)
+        // {
+        //     Destination myDestination = new Destination(cityName, description);
+        //     return RedirectToAction("Index");
+        // }
 
-        [HttpGet("/destinations/{id}")]
-        public ActionResult Show(int id)
-        {
-            Destination foundDestination = Destination.Find(id);
-            return View(foundDestination);
-        }
+        // // [HttpGet("/destinations/{id}")]
+        // // public ActionResult Show(int id)
+        // // {
+        // //     Destination foundDestination = Destination.Find(id);
+        // //     return View(foundDestination);
+        // // }
 
+        [HttpGet("/categories/{categoryId}/destinations/{destinationId}")]
+        public ActionResult Show(int categoryId, int destinationId)
+        {
+            Destination destination = Destination.Find(destinationId);
+            Category category = Category.Find(categoryId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("destination", destination);
+            model.Add("category", category);
+            return View(model);
+        }
     }
 }
